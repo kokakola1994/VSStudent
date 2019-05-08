e698078d3a3d89970c901ffbe57b685267a403fb
Mykola Klymchuk <zabohunter1994@gmail.com>
2019-05-08 14:14:26 +0200

    FunkcjaShablonowa bez rejestracii i sms

--- source/repos/FunkcjaShablonowa/FunkcjaShablonowa/FunkcjaShablonowa.cpp
+++ source/repos/FunkcjaShablonowa/FunkcjaShablonowa/FunkcjaShablonowa.cpp
@@ -7,84 +7,105 @@
 
 using namespace std;
 
+//funkcja wyswietlenia dowolnej tablicy
 template <typename T>
-void printArray(const T*array, int count)
+void printArray(const T* array, int count)
 {
-	static_assert (is_pod<T>::value, "type T mucie byc prostym");
+	static_assert(std::is_pod<T>::value, "typ T musi byc prostym.");
+
 	for (int ix = 0; ix < count; ix++)
 		cout << array[ix] << " ";
 	cout << endl;
-	template < typename T = int >
-	void F(T*arr, size_t N = 10, T t = T(5))
-	{
-		for (size_t i = 0; i < N; i++)
-			arr[i] *= t;
-	}
-
-	template < typename RT = double, typename T = int >
-	struct func
-	{
-		RT operator () (const T& a, const T& t)
-		{
-			return (RT)a / t;
-		}
-	};
-	template <typename RT = double, typename T = int >
-	struct func2
-	{
-		RT operator () (const T& a, const T& t)
-		{
-			return (RT)a * t;
-		}
-	};
 }
 
-template <typename RT = double, typename T = int, class Func = func<RT,T> >
-RT serGeometr(T*arr, size_t N = 10, T t = T(5), Func f = func<RT, T>())
+
+//zad1. wariant1. przyklad sparametryzowanej funkcji
+template<typename T = int>
+
+void F(T * arr, size_t N = 10, T t = T(5))
+{
+	for (size_t i = 0; i < N; i++)
+		arr[i] *= t;
+}
+
+//za1. wariant2. dwa funktory
+template<typename RT = double, typename T = int>
+struct func
+{
+	RT operator()(const T& a, const T& t)
+	{
+		return(RT)a / t;
+	}
+};
+
+template<typename RT = double, typename T = int>
+struct func2
+{
+	RT operator()(const T& a, const T& t)
+	{
+		return(RT)a* t;
+	}
+};
+
+
+template<typename RT = double, typename T = int, class Func = func<RT, T>>
+RT SetGeometr(T * arr, std::size_t N = 10, T t = T(5), Func f = func < RT, T>())
 {
 	RT res = 0;
-	for (size_t i = 0; i < N; i++)
-		res += (arr[i], t);
+	for (std::size_t i = 0; i < N; i++)
+		res += f(arr[i], t);
 	return pow(abs(res), 1.0 / N);
 }
-template <class T>
+
+//zad2. funktor i uogolniony algorytm
+template<class T>
 class Positive
 {
 public:
-	bool operator() (const T& t)
+	bool operator()(const T& t)
 	{
-		return bool(> T(0));
-
+		return bool(t > T(0));
 	}
 };
-template <class InputIterator, class OutputIterator, class Pridicate>
-void reverse_copy_if(InputIterator first, InputIterator last, OutputIterator result, OutputIterator Functor)
+template<class InputIterator, class OutputIterator, class Pridicate>
+void reverse_copy_if(InputIterator first, InputIterator last, OutputIterator result, Pridicate Functor)
 {
-	for(; last >= first; last --)
+	for (; last >= first; last--)
 		if (Functor(*last))
 		{
 			*result = *last; result++;
 		}
 }
-
 int main()
 {
 	int iArray[] = { 1,2,3,4,5,6,7,8,9,10 };
-	double dAraay[] = { 1.23,2.23,3.23,4,23,5.23,6.67,7.78,8.89,9.89 };
+	double dArray[] = { 1.2345,2.2355,3.2553,4.555,5.555,6.777,7.888,8.444 };
+
+	//wywolanie z ustawieniami domyslnymi
 	F(iArray);
-	cout << "\n Tablica typu int; \n"; printArray(iArray, sizeof(iArray) / sizeof(int));
-	F(dAraay, 7.6, 6.7);
-	cout << "\n Tablica typu double; \n"; printArray(dArray, sizeof(dAraay) / sizeof(double));
-	cout << "domyslnie, func: " << serGeometr(iArray) << endl;
-	cout << "func 2: " << serGeometr<double, double, func<double, double>>(dAraay, sizeof(dAraay) / sizeof(double), 12.0, func2<double, double>())<< endl;
+	cout << "\nTablica typu int:\n"; printArray(iArray, sizeof(iArray) / sizeof(int));
+	//wywolanie z parametrami
+	F(dArray, 7u, 6.7);
+	cout << "\nTablica typu double:\n"; printArray(dArray, sizeof(dArray) / sizeof(double));
 
+	//zad1.wariant2. przyklad sparametyrowaznej funkcji, ktora wykonuje dzialanie
+	//na dowlonej tablicy i zwraca liczbe dowolnego typu
+
+	//wywoalnie z ustawieniami domyslnymi
+	cout << "\ndomyslnie, func:\n" << SetGeometr(iArray) << endl;
+	//wywolanie z prametrami i funktorem fun2
+	cout << "\nfunc2:\n" << SetGeometr<double, double, func2<double,
+		double>>(dArray, sizeof(dArray) / sizeof(double), 12.0, func2<double, double>()) << endl;
+
+	//zad2. funktor i uogolniony algorytm
 	int res[10] = { 0 };
-	reverse_copy_if(iArray, iArray + 10, res, Positive<int>())
-		printArray(res, 10);
+	reverse_copy_if(iArray, iArray + 10, res, Positive<int>());
+	printArray(res, 10);
 
+	//zad2. funktor i uogolniony algorytm
 	double res2[7] = { 0.0 };
-	reverse_copy_if(dArray, dArray + 7, res, Positive<double>())
-		printArray(res2, 7);
+	reverse_copy_if(dArray, dArray + 7, res2, Positive<double>());
+	printArray(res2, 7);
 
 	system("pause");
 	return 0;
