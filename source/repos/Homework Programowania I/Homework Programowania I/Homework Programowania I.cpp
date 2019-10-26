2925ee52b04467b6d6c0c3b221eba38a03cf9efa
Mykola Klymchuk <zabohunter1994@gmail.com>
2019-10-26 17:37:06 +0200

    Silnia zadanie 1

--- source/repos/Homework Programowania I/Homework Programowania I/Homework Programowania I.cpp
+++ source/repos/Homework Programowania I/Homework Programowania I/Homework Programowania I.cpp
@@ -33,7 +33,7 @@ int main()
 	//
 	//return 0;
 
-	
+
 	// koniec pirwszego zadania
 
 	//Zadanie N 2
@@ -76,62 +76,62 @@ int main()
 
 	return 0;
 */
-	//koniec zadania 3
+//koniec zadania 3
 
-	//Zadanie 4
+//Zadanie 4
 
-	/*int a;
+/*int a;
 
-	printf("");
-	
-	for (int i = 0; i < 3; i++)
+printf("");
+
+for (int i = 0; i < 3; i++)
+
+{
+	scanf_s("%d", &a);
+
+	if (a < 0) printf("minus\n");
+
+	else printf("plus\n");
+}*/
+//koniec czwartego zadania
+
+//Zadanie 5
+
+/*for (int i = 0; i < 4; i++)
+{
+	int N;
+
+	int A, B, rez;
+
+	printf("Wprowadz numer od 1 do 4\n");
+
+	scanf_s("%i", &N);
+
+	scanf_s("%d", &A);
+
+	scanf_s("%d", &B);
+
+	switch (N)
 
 	{
-		scanf_s("%d", &a);
 
-		if (a < 0) printf("minus\n");
+	case 1: rez = A + B; printf("Dodawanie = %d \n", rez); break;
 
-		else printf("plus\n");
-	}*/
-	//koniec czwartego zadania
+	case 2: rez = A - B; printf("Odejmowanie = %d \n", rez); break;
 
-	//Zadanie 5
+	case 3: rez = A * B; printf("Mnozenie = %d \n", rez); break;
 
-	/*for (int i = 0; i < 4; i++)
-	{
-		int N;
+	case 4: rez = A / B; printf("Dzielienie = %d \n", rez); break;
 
-		int A, B, rez;
-
-		printf("Wprowadz numer od 1 do 4\n");
-
-		scanf_s("%i", &N);
-
-		scanf_s("%d", &A);
-
-		scanf_s("%d", &B);
-
-		switch (N)
-
-		{
-
-		case 1: rez = A + B; printf("Dodawanie = %d \n", rez); break;
-
-		case 2: rez = A - B; printf("Odejmowanie = %d \n", rez); break;
-
-		case 3: rez = A * B; printf("Mnozenie = %d \n", rez); break;
-
-		case 4: rez = A / B; printf("Dzielienie = %d \n", rez); break;
-		
-		default: printf("blend\n"); break;
-
-		}
+	default: printf("blend\n"); break;
 
 	}
 
-	system("pause");
+}
 
-	return 0;
+system("pause");
+
+return 0;
 */
 
 //int ile;
@@ -165,20 +165,39 @@ int main()
 //}
 //printf("min = %lf max = %lf\n", min, max);
 
-int n;
-double a;
-printf("podaj a rzeczywiste oraz n calkowite\n");
-scanf_s("%lf %d", &a, &n);
-double sum = 1, pot = a;
-int minus = -1;
-for (int i = 0; i < n; i++)
-{
-	sum += minus * pot;
-	minus *= -1;
-	pot *= a;
+//int n;
+//double a;
+//printf("podaj a rzeczywiste oraz n calkowite\n");
+//scanf_s("%lf %d", &a, &n);
+//double sum = 1, pot = a;
+//int minus = -1;
+//for (int i = 0; i < n; i++)
+//{
+//	sum += minus * pot;
+//	minus *= -1;
+//	pot *= a;
+//}
+//printf("%lf\n", sum);
+/*
+	int n, i;
+	unsigned long long silna = 1;
+	printf("Wpisz integer: ");
+	scanf_s("%d", &n);
+
+	if (n < 0)
+		printf("Error! Silna nie istnieje");
+	else
+	{
+		for (i = 1; i <= n; ++i)
+		{
+			silna *= i;
+		}
+		printf("Factorial of %d = %llu", n, silna);
+	}
+	*/
+
 }
-printf("%lf\n", sum);
-}
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
